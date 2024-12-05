6789605141a546747ca912b43a8b7966b92b3e33
George Sanger <paperclipstudio@gmail.com>
2024-12-05 21:30:41 +0000

    Day2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -2,65 +2,116 @@ use std::env;
 use std::fs;
 
 pub fn part1() {
-    let contents = fs::read_to_string("src/day2.txt")
-        .expect("Should have been able to read the file");
-    let list_2d = contents.lines()
+    let contents =
+        fs::read_to_string("src/day2.txt").expect("Should have been able to read the file");
+    let list_2d = contents
+        .lines()
         .map(|line| {
-            line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>()
-        }).collect::<Vec<_>>();
+            line.split(" ")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
 
-    let increasing = list_2d.iter().map(|line| {
-        line.windows(2).map(|xs| xs[0] < xs[1]).all(|x| x)
-    }).collect::<Vec<_>>();
+    let solved2 = list_2d.iter().filter(|xs| is_safe(xs)).count();
 
-    let decreasing = list_2d.iter().map(|line| {
-        line.windows(2).map(|xs| xs[0] > xs[1]).all(|x| x)
-    }).collect::<Vec<_>>();
-
-    let at_least_1 = list_2d.iter().map(|line| {
-        line.windows(2).map(|xs| (xs[0] - xs[1]).abs() > 0).all(|x| x)
-    }).collect::<Vec<_>>();
-
-    let at_most_3 = list_2d.iter().map(|line| {
-        line.windows(2).map(|xs| (xs[0] - xs[1]).abs() < 4).all(|x| x)
-    }).collect::<Vec<_>>();
-
-        println!("{decreasing:?}");
-        println!("{increasing:?}");
-        println!("{at_least_1:?}");
-        println!("{at_most_3:?}");
-
-
-    let solved = increasing.iter().zip(decreasing).map(|(a,b)| a | b)
-        .zip(at_least_1).map(|(a,b)| a & b)
-        .zip(at_most_3).map(|(a,b)| a & b).filter(|x| *x).count();
-
-    println!("{solved:?}");
-
-
-
+    println!("{solved2:?}");
 }
 
-pub fn _day1_2() {
-    let contents = fs::read_to_string("src/day1.txt")
-        .expect("Should have been able to read the file");
-    let mut sum = 0;
-    let mut firsts = Vec::new();
-    let mut seconds = Vec::new();
-    for line in contents.lines() {
-        let pair: Vec<_> = line.split("   ").collect();
-        firsts.push(pair[0].parse::<i32>().unwrap());
-        seconds.push(pair[1].parse::<i32>().unwrap());
+pub fn part2() {
+    let contents =
+        fs::read_to_string("src/day2.txt").expect("Should have been able to read the file");
+    let list_2d = contents
+        .lines()
+        .map(|line| {
+            line.split(" ")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let solved2 = list_2d.iter().filter(|xs| is_safe_buffer(xs)).count();
+
+    println!("{solved2:?}");
+}
+
+fn is_safe_buffer(items: &[i32]) -> bool {
+    if is_safe(items) {
+        return true;
     }
 
-    for id in firsts.iter() {
-        sum += seconds.iter().filter(|x| *x == id).count() as i32 * id;
+    for item in 0..items.len() {
+        let items_without_one: Vec<i32> = items[..item]
+            .iter()
+            .chain(items[item + 1..].iter())
+            .inspect(|x| {
+                print!("{x:?}, ");
+            })
+            .copied()
+            .collect();
+        println!();
+        if is_safe(&items_without_one) {
+            return true;
+        }
     }
 
+    false
+}
 
-    println!("{sum:?}");
+fn is_safe(items: &[i32]) -> bool {
+    (items.windows(2).all(|xs| xs[0] < xs[1]) || items.windows(2).all(|xs| xs[0] > xs[1]))
+        && items.windows(2).all(|xs| (xs[0] - xs[1]).abs() >= 0)
+        && items.windows(2).all(|xs| (xs[0] - xs[1]).abs() <= 3)
+}
 
-    // A1 - B1 + A2 - B2 == A2 - B1 + A1 - B2
-    // So we don't need to sort the lists
+#[cfg(test)]
+mod test {
+    use super::*;
 
+    #[test]
+    fn increasing() {
+        assert!(is_safe(&[1, 2, 3, 4, 5]));
+        assert!(!is_safe(&[5, 2, 3, 4, 5]));
+        assert!(!is_safe(&[5, 2, 3, 4, 5]));
+    }
+
+    #[test]
+    fn decreasing() {
+        assert!(is_safe(&[5, 4, 3, 2, 1]));
+        assert!(!is_safe(&[5, 2, 3, 4, 5]));
+        assert!(!is_safe(&[5, 2, 3, 4, 5]));
+    }
+
+    #[test]
+    fn at_least_1() {
+        assert!(is_safe(&[5, 7, 10, 12, 13]));
+        assert!(!is_safe(&[5, 5, 3, 2, 1]));
+        assert!(!is_safe(&[5, 2, 3, 4, 5]));
+        assert!(!is_safe(&[5, 2, 3, 4, 5]));
+    }
+
+    #[test]
+    fn at_most_3() {
+        assert!(is_safe(&[5, 7, 10, 12, 13]));
+        assert!(!is_safe(&[3, 8, 9, 10, 11]));
+        assert!(!is_safe(&[10, 6, 5, 4, 3, 2]));
+    }
+
+    #[test]
+    fn part2_sample4() {
+        assert!(is_safe_buffer(&[1, 3, 2, 4, 5]));
+    }
+
+    #[test]
+    fn part2_sample5() {
+        assert!(is_safe_buffer(&[8, 6, 4, 4, 1]));
+    }
+
+    #[test]
+    fn part2_samples() {
+        assert!(is_safe_buffer(&[7, 6, 4, 2, 1]));
+        assert!(!is_safe_buffer(&[1, 2, 7, 8, 9]));
+        assert!(!is_safe_buffer(&[9, 7, 6, 2, 1]));
+        assert!(is_safe_buffer(&[1, 3, 6, 7, 9]));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ mod day2;
 
 fn main() {
     println!("Hello, world!");
-    day2::part1();
+    day2::part2();
 }
